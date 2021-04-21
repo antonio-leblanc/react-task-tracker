1e2abb5ef19d468ab87f8a602407f397ccad6181
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-21 12:47:27 -0300

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {useState} from 'react'
 import './App.css';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Program new task manager',
+    },
+    {
+      id:2,
+      text: 'Create new react site',
+    },
+  ])
+
   return (
     <div className="container">
       <Header title='Task Tracker'></Header>
+      <Tasks tasks={tasks}></Tasks>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+
+const Tasks = ({tasks}) => {
+  return (
+    <>
+      {tasks.map((t) => (
+        <h3 key={t.id}>{t.text}</h3>
+      ))}
+    </>
+  )
+}
+
+export default Tasks
