d93a4cf064ea698337d2eb9998155ae42bed58f4
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-21 12:29:07 -0300

    propTypes + pass function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,29 @@
 import Button from './Button'
 
 const Header = (props) => {
+
+  const onClick = () => {
+    console.log('Clicked')
+  }
+
   return (
     <header className='header'>
       <h1>{props.title}</h1>
-      <Button color='green' text='Add'></Button>
+      <Button color='green' text='Add' onClick={onClick}></Button>
     </header>
   )
 }
 
 Header.defaultProps = {
-  title : "TaskTracker"
+  title : "Task Tracker"
 }
 
 // CSS IN JS if we want dynamic css
-{/* <h1 style={headingStyle}>Task Tracker {props.title}</h1>
+/* <h1 style={headingStyle}>Task Tracker {props.title}</h1>
 
 const headingStyle = {
   color:'green',
   backgroundColor: 'yellow'
-} */}
+} */
 
 export default Header
